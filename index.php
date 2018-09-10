f7964eea928f01f53e2aa15e7da6cd3e086671ec
Kristian Paekivi <krpaeki@users.noreply.github.com>
2018-09-10 10:41:00 +0300

    html muudetud php-ks

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Intro to something</title>
+	<style>
+	
+	img:hover {
+		-webkit-filter: drop-shadow(8px 8px 10px green);
+		filter: drop-shadow(8px 8px 10px green);}
+	
+	a {
+		text-decoration: none;
+		color: black;
+		}
+		
+	a:link, a:visited  
+	{    color: hotpink; }
+	
+	
+	a:hover {
+    color: black;}
+	
+	
+	</style>
+</head>
+<body>
+	<h1>HEHEHHEHEHEeee</h1>
+	<p>Siin on minu õppetöö raames tehtud veebilehed. Need ei oma mingit tähtsust ja kopeerimisel pole mõtet.</p>
+	<p>Lisan rea kodutöö eesmärgil</p>
+	<p>See leht on valminud <a href="http://www.tlu.ee" target="_blank">TLÜ</a> õppetöö raames.</p>
+	<p>Mul on ka sõber, kes teeb ka <a href="../~raimpre"> veebi</a></p>
+	
+	<img src="../gnr2bnot2bin2bthis2blifetime.png" alt= "Gnr logo"
+	> 
+	<img src="../gnfr.png" alt= "GnFnR" Height=300 width=300
+	>
+	<!--<img src="http://www.gunsandrosestweettour.com/images/gnfr.svg" alt= "GnFnR" Height=300 width=300
+	>-->
+</body>
+</html>
